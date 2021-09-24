769a90131d5fef5ce06273ab4a3ca036211c0868
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2021-09-25 02:04:01 +0500

    changes in chart router

--- models/chart.model.js
+++ models/chart.model.js
@@ -1,6 +1,6 @@
 const chart = [
-  { label: "testing1", data: [12, 49, 23, 65, 32, 53] },
-  { label: "testingc2", data: [22, 59, 13, 25, 12, 73] },
+  { label: "testing1", data: [12, 12, 23, 65, 32, 53] },
+  { label: "testingc2", data: [22, 12, 13, 25, 12, 73] },
 ];
 
 module.exports = chart;
